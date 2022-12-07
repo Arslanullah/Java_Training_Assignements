523a2da0eed6810146554013478409ef206a2f9f
Arslanullah <114203538+Arslanullah@users.noreply.github.com>
2022-12-06 18:43:37 -0600

    My first commit Of the Book Encapsulation Task And its Test Class As well

---
+++ src/Day27_EncapSulation_OOP/Book.java
@@ -0,0 +1,103 @@
+package Day27_EncapSulation_OOP;
+
+public class Book {
+
+
+    private String title;
+
+    private String type;
+
+    private String author;
+
+    private double price;
+
+
+    public Book(String title, String type, String author, double price) {
+        setTitle(title);
+        setType(type);
+        setAuthor(author);
+        setPrice(price);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price<=0){
+            System.err.println("Error Wrong Prices ");
+            System.exit(0);
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+ create a class named Book:
+			variables:
+				title, type, author, price
+
+			Encapsulate all the fields
+					Conditions:
+						1. price can not be set to negative
+
+			Add a constructor that can set all the fields
+
+			Methods:
+				toString()
+
+
+ */
---
+++ src/Day27_EncapSulation_OOP/Test_Book.java
@@ -0,0 +1,15 @@
+package Day27_EncapSulation_OOP;
+
+public class Test_Book {
+    public static void main(String[] args) {
+
+
+        Book book=new Book("Holy Quran","EnglishVersion","Mohammad Pickhtel",154);
+
+        System.out.println(book);
+
+
+
+
+    }
+}
